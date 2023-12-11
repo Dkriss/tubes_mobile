f357d9ca00fe2380b57ed4605d7360bfa49f4748
Dkriss <89033170+Dkriss@users.noreply.github.com>
2023-12-11 08:48:21 +0700

    ngrapihno

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tubes_mobile/presentation/view/landing_page.dart';
 
-void main(){
-  runApp(const MyApp());
-}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
